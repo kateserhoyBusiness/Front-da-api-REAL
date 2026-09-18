2b731038532c63947af970c5827a88934217754b
kateserhoyBusiness <kateserhoy@gmail.com>
2026-09-18 16:46:36 -0300

    Delete front-blog-angularjs-master directory

--- front-blog-angularjs-master/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('blog', []);
-angular.module('blog').controller('Rest', function ($scope, $http){
-  $http.get('https://api-real-blog.onrender.com/categorias').
-      success(function(data) {
-          $scope.publicacoes = data;
-      }
-  );
-});
